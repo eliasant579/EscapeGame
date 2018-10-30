a689a39a310da0bd11bf37ca9c68c39d8dfd656a
eliasant579 <eliasant579@ed.amdsb.ca>
2018-10-29 21:38:31 -0700

    Missing output and random scenes

--- EscapeGame/Form1.Designer.cs
+++ EscapeGame/Form1.Designer.cs
@@ -28,13 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mainOutputLabel = new System.Windows.Forms.Label();
+            this.redAnswerLabel = new System.Windows.Forms.Label();
+            this.blueAnswerLabel = new System.Windows.Forms.Label();
+            this.yellowAnswerLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.yellowLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mainOutputLabel
+            // 
+            this.mainOutputLabel.AutoSize = true;
+            this.mainOutputLabel.Location = new System.Drawing.Point(55, 27);
+            this.mainOutputLabel.Name = "mainOutputLabel";
+            this.mainOutputLabel.Size = new System.Drawing.Size(531, 51);
+            this.mainOutputLabel.TabIndex = 0;
+            this.mainOutputLabel.Text = "You are in jail. \r\nIt\'s nightime and you\' ve just wake up in pain because sometin" +
+    "g was thrown at you. \r\nDo you shout angrily to the other prisoners?";
+            // 
+            // redAnswerLabel
+            // 
+            this.redAnswerLabel.AutoSize = true;
+            this.redAnswerLabel.Location = new System.Drawing.Point(125, 165);
+            this.redAnswerLabel.Name = "redAnswerLabel";
+            this.redAnswerLabel.Size = new System.Drawing.Size(32, 17);
+            this.redAnswerLabel.TabIndex = 1;
+            this.redAnswerLabel.Text = "Yes";
+            // 
+            // blueAnswerLabel
+            // 
+            this.blueAnswerLabel.AutoSize = true;
+            this.blueAnswerLabel.Location = new System.Drawing.Point(125, 227);
+            this.blueAnswerLabel.Name = "blueAnswerLabel";
+            this.blueAnswerLabel.Size = new System.Drawing.Size(26, 17);
+            this.blueAnswerLabel.TabIndex = 2;
+            this.blueAnswerLabel.Text = "No";
+            // 
+            // yellowAnswerLabel
+            // 
+            this.yellowAnswerLabel.AutoSize = true;
+            this.yellowAnswerLabel.Location = new System.Drawing.Point(128, 282);
+            this.yellowAnswerLabel.Name = "yellowAnswerLabel";
+            this.yellowAnswerLabel.Size = new System.Drawing.Size(0, 17);
+            this.yellowAnswerLabel.TabIndex = 3;
+            // 
+            // redLabel
+            // 
+            this.redLabel.BackColor = System.Drawing.Color.Red;
+            this.redLabel.Location = new System.Drawing.Point(55, 159);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(64, 23);
+            this.redLabel.TabIndex = 4;
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.BackColor = System.Drawing.Color.Blue;
+            this.blueLabel.Location = new System.Drawing.Point(55, 221);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(64, 23);
+            this.blueLabel.TabIndex = 5;
+            // 
+            // yellowLabel
+            // 
+            this.yellowLabel.BackColor = System.Drawing.Color.Gold;
+            this.yellowLabel.Location = new System.Drawing.Point(55, 276);
+            this.yellowLabel.Name = "yellowLabel";
+            this.yellowLabel.Size = new System.Drawing.Size(64, 23);
+            this.yellowLabel.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.yellowLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.yellowAnswerLabel);
+            this.Controls.Add(this.blueAnswerLabel);
+            this.Controls.Add(this.redAnswerLabel);
+            this.Controls.Add(this.mainOutputLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label mainOutputLabel;
+        private System.Windows.Forms.Label redAnswerLabel;
+        private System.Windows.Forms.Label blueAnswerLabel;
+        private System.Windows.Forms.Label yellowAnswerLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label yellowLabel;
     }
 }
 
--- EscapeGame/Form1.cs
+++ EscapeGame/Form1.cs
@@ -12,9 +12,142 @@ namespace EscapeGame
 {
     public partial class Form1 : Form
     {
+        int scene = 0;
+        bool angerValue = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            // scenes 1 & 8 & fights & KO are not yes/no decisions. Managed differently
+
+            if (e.KeyCode == Keys.R)                //red button (left)
+            {
+                switch (scene)
+                {
+                    case 0:
+                        angerValue = true;
+                        scene = 1;
+                        break;
+                    case 3:
+                        scene = 6;
+                        break;
+                    case 5:
+                        scene = 8;
+                        break;
+                    case 9:
+                        scene = 8;
+                        break;
+                    case 11:
+                        scene = 12;
+                        break;
+
+                }
+            }
+            else if (e.KeyCode == Keys.B)           //blue button (right)
+            {
+                switch (scene)
+                {
+                    case 0:
+                        scene = 1;
+                        break;
+                    case 2:
+                        scene = 5;
+                        break;
+                    case 3:
+                        scene = 7;
+                        break;
+                    case 5:
+                        scene = 9;
+                        break;
+                    case 9:
+                        scene = 11;
+                        break;
+                    case 11:
+                        scene = 14;
+                        break;
+                }
+            }
+            else if (e.KeyCode == Keys.Y)           //yellow button
+            {
+                if (scene == 11)
+                {
+                    scene = 13;
+                }
+                else
+                {
+                    //Oak's words echoed... There's a time and place for everything, but not now.
+                    return;
+                }
+            }
+
+            switch (scene)
+            {
+                case 0:
+                    break;
+                case 1:
+                    mainOutputLabel.Text = "Some stuff";
+                    redAnswerLabel.Text = "Ye";
+                    blueAnswerLabel.Text = "yay";
+                    break;
+                case 2:
+                    mainOutputLabel.Text = "Some stuff";
+                    //80% fight, to scene 3 or other case (5 or 7)
+                    break;
+                case 3:
+                    mainOutputLabel.Text = "Some stuff";
+                    redAnswerLabel.Text = "Ye";
+                    blueAnswerLabel.Text = "yay";
+                    break;
+                case 4:
+                    mainOutputLabel.Text = "Some stuff";
+                    //end
+                    break;
+                case 5:
+                    mainOutputLabel.Text = "Some stuff";
+                    redAnswerLabel.Text = "Ye";
+                    blueAnswerLabel.Text = "yay";
+                    break;
+                case 6:
+                    mainOutputLabel.Text = "Some stuff";
+                    break;
+                case 7:
+                    mainOutputLabel.Text = "Some stuff";
+                    //end
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    mainOutputLabel.Text = "Some stuff";
+                    redAnswerLabel.Text = "Ye";
+                    blueAnswerLabel.Text = "yay";
+                    break;
+                case 10:
+                    mainOutputLabel.Text = "Some stuff";
+                    //end
+                    break;
+                case 11:
+                    mainOutputLabel.Text = "Some stuff";
+                    redAnswerLabel.Text = "Ye";
+                    blueAnswerLabel.Text = "yay";
+                    break;
+                case 12:
+                    mainOutputLabel.Text = "Some stuff";
+                    //end
+                    break;
+                case 13:
+                    mainOutputLabel.Text = "Some stuff";
+                    //end
+                    break;
+                case 14:
+                    mainOutputLabel.Text = "Some stuff";
+                    //end
+                    break;
+            }
         }
     }
 }
