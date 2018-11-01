2fd2418f4871f9fbdc6b56439dbe68113413f851
eliasant579 <eliasant579@ed.amdsb.ca>
2018-11-01 14:50:36 -0700

    Output is totally wrong in most of the code. Some decisions are backwards Program wotking tho

--- EscapeGame/Form1.cs
+++ EscapeGame/Form1.cs
@@ -24,7 +24,9 @@ namespace EscapeGame
     public partial class Form1 : Form
     {
         int scene = 0;
+        int i = 0;
         bool angerValue = false;
+        Random randGen = new Random();
 
         public Form1()
         {
@@ -48,9 +50,26 @@ namespace EscapeGame
                         scene = 1;
                         break;
                     case 2:
-                        //reciving input (perhaps) from satisfied if statement in textbox event
-                        mainOutputLabel.Text = "You got this!";
-                        //80% fight, to scene 3 or other case (5 or 7)
+                        //coming from textbox event
+                        mainOutputLabel.Text = "There is an high probability of a fight";
+                        Thread.Sleep(2000);
+                        if (randGen.Next(1, 101) <= 80)
+                        {
+                            mainOutputLabel.Text = "A fight is starting!";
+                            Thread.Sleep(1000);
+                            if (randGen.Next(1, 101) <= 50 || angerValue == true)
+                            {
+                                scene = 4;
+                            }
+                            else
+                            {
+                                scene = 5;
+                            }
+                        }
+                        else
+                        {
+                            scene = 5;
+                        }
                         break;
                     case 3:
                         scene = 6;
@@ -76,6 +95,7 @@ namespace EscapeGame
                         scene = 1;
                         break;
                     case 2:
+                        //coming from textbox event
                         scene = 5;
                         break;
                     case 3:
@@ -114,7 +134,7 @@ namespace EscapeGame
                     blueLabel.Visible = true;
                     blueAnswerLabel.Visible = true;
                     yellowLabel.Visible = false;
-                    return;
+                    //return;
                 }
             }
 
@@ -133,55 +153,75 @@ namespace EscapeGame
                     mainOutputLabel.Text = "Which of the key is the right one?";
                     break;
                 case 2:
-                    //free or not?
+                    //scene moved inside textbox event
                     break;
                 case 3:
-                    mainOutputLabel.Text = "Some stuff";
-                    redAnswerLabel.Text = "Ye";
-                    blueAnswerLabel.Text = "yay";
+                    mainOutputLabel.Text = "You continue with the help of the other prisoners. Do you go left or right?";
+                    redAnswerLabel.Text = "Right";
+                    blueAnswerLabel.Text = "Left";
                     break;
                 case 4:
-                    mainOutputLabel.Text = "Some stuff";
+                    mainOutputLabel.Text = "You have been knocked out during the fight";
+                    if (angerValue == true)
+                    {
+                        mainOutputLabel.Text += ":\nthe other prisoners were anry with you because you woke them up yelling.";
+                    }
+                    else
+                    {
+                        mainOutputLabel.Text += ".";
+                    }
                     //end
                     break;
                 case 5:
-                    mainOutputLabel.Text = "Some stuff";
-                    redAnswerLabel.Text = "Ye";
-                    blueAnswerLabel.Text = "yay";
+                    mainOutputLabel.Text = "You continue alone. Do you go right or left?";
+                    redAnswerLabel.Text = "Right";
+                    blueAnswerLabel.Text = "Left";
                     break;
                 case 6:
-                    mainOutputLabel.Text = "Some stuff";
-                    break;
+                    mainOutputLabel.Text = "You find a door and together you manage to knock it down";
+                    goto case 11;
+                    //scene = 11;
+                    //break;
                 case 7:
-                    mainOutputLabel.Text = "Some stuff";
+                    mainOutputLabel.Text = "The prisoners you just set free are too noisy:\n" +
+                        "a bunch of officers notice you and you are all forced inside your cells.";
                     //end
                     break;
                 case 8:
+                    mainOutputLabel.Text = "A officer notice you, act fast!\n" +
+                        "Ther's a door in front of you, find the right key!";
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    redAnswerLabel.Text = null;
+                    blueAnswerLabel.Text = null;
+                    keysTextBox.Visible = true;
                     break;
                 case 9:
-                    mainOutputLabel.Text = "Some stuff";
-                    redAnswerLabel.Text = "Ye";
-                    blueAnswerLabel.Text = "yay";
+                    mainOutputLabel.Text = "You find yourself inside the main office.\n" +
+                        "Do you run the other way or you dissimulate and walk past?";
+                    redAnswerLabel.Text = "Run back!";
+                    blueAnswerLabel.Text = "Walk past";
                     break;
                 case 10:
-                    mainOutputLabel.Text = "Some stuff";
-                    //end
+                    //this scene has been moved in the text box event
                     break;
                 case 11:
-                    mainOutputLabel.Text = "Some stuff";
+                    //yellowAnswerLabel
+                    mainOutputLabel.Text = "You are finally outside the prison! Which way do you go?";
                     redAnswerLabel.Text = "Ye";
                     blueAnswerLabel.Text = "yay";
                     break;
                 case 12:
-                    mainOutputLabel.Text = "Some stuff";
+                    mainOutputLabel.Text = "You end up in the courthouse. \nAgain.";
                     //end
                     break;
                 case 13:
-                    mainOutputLabel.Text = "Some stuff";
+                    mainOutputLabel.Text = "You end up in a curch and become a minister to escape the police.";
                     //end
                     break;
                 case 14:
-                    mainOutputLabel.Text = "Some stuff";
+                    mainOutputLabel.Text = "You end up in Stratford Central Secondary School.\n" +
+                        "At least it's not St.Mike's";
                     //end
                     break;
             }
@@ -190,7 +230,7 @@ namespace EscapeGame
         
         private void keysTextBox_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter)
+            if (e.KeyCode == Keys.Enter && scene == 1)
             {
                 if (keysTextBox.Text == RIGHT_KEY_S1)
                 {
@@ -198,17 +238,40 @@ namespace EscapeGame
                     blueLabel.Visible = true;
                     keysTextBox.Text = null;
                     keysTextBox.Visible = false;
+                    scene = 2;                      //this is scene two;
                     mainOutputLabel.Text = "Right key! \nDo you want to set the other prisoners free?";
                     redAnswerLabel.Text = "Yes";
                     blueAnswerLabel.Text = "No";
-                    scene = 2;
-                    this.Focus();
-                    //how do I leave this method and go back to from1 keydown?
+                    this.Focus();                   //leave this method
                 }
                 else
                 {
                     redAnswerLabel.Text = "Wrong key or invalid input.";
                     keysTextBox.Text = null;
+                }
+            }
+            if (e.KeyCode == Keys.Enter && scene == 8)
+            {
+                if (keysTextBox.Text == RIGHT_KEY_S8)
+                {
+                    redLabel.Visible = true;
+                    blueLabel.Visible = true;
+                    keysTextBox.Text = null;
+                    keysTextBox.Visible = false;
+                    scene = 11;
+                    mainOutputLabel.Text = "Right key!\n" +
+                        "Press any key to continue.";
+                    this.Focus();                   //leave this method
+                }
+                else
+                {
+                    i++;
+                    if (i == 3)
+                    {
+                        //scene 10
+                        mainOutputLabel.Text = "You have been caught and brought back to your cell.";
+                        //end
+                    }
                 }
             }
         }
